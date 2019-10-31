56a2f8ccbeaf29e174c9b8d31e51e61285cfde70
e-11191 <51792218+e-11191@users.noreply.github.com>
2019-10-31 10:10:50 -0700

    Add files via upload

---
+++ JSFinalGIT/js/main.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+var firebaseConfig = {
+    apiKey: "AIzaSyAyuKIWSz0Zq3ZwCiOhR2HVYMTa4Qume4g",
+    authDomain: "reservation-site-e87e9.firebaseapp.com",
+    databaseURL: "https://reservation-site-e87e9.firebaseio.com",
+    projectId: "reservation-site-e87e9",
+    storageBucket: "reservation-site-e87e9.appspot.com",
+    messagingSenderId: "964128789580",
+    appId: "1:964128789580:web:ec9f9a0d8891aa5fe650e9",
+    measurementId: "G-9LZYSYQ3ZJ"
+  };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+//Connect to the database
+var database = firebase.database();
+
